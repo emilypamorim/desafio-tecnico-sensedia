4dbbee89430acede162ead4b98a83d26dca4ad0a
Emily Amorim <158009884+emiamorim@users.noreply.github.com>
2026-01-18 12:14:48 -0300

    feat: estrutura inicial do projeto e configurações

---
+++ src/test/java/emily/amorim/desafio_tecnico/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package emily.amorim.desafio_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
