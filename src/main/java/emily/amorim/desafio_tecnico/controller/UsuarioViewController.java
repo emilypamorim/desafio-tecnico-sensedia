8768cf6d7768ca25c37ba5c4a3d4ee6c823cd691
Emily Amorim <158009884+emiamorim@users.noreply.github.com>
2026-01-19 17:02:36 -0300

    feat: tela de listagem de usuários

--- src/main/java/emily/amorim/desafio_tecnico/controller/UsuarioViewController.java
+++ src/main/java/emily/amorim/desafio_tecnico/controller/UsuarioViewController.java
@@ -1,12 +1,15 @@
 package emily.amorim.desafio_tecnico.controller;
 
 import emily.amorim.desafio_tecnico.entity.Usuario;
+import emily.amorim.desafio_tecnico.repository.UsuarioRepository;
 import emily.amorim.desafio_tecnico.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/usuario")
@@ -23,7 +26,13 @@ public class UsuarioViewController {
     @PostMapping("/enviar")
     public String enviaForm(Usuario usuario){
         service.create(usuario);
-        return "redirect:/usuario/cadastro";
+        return "redirect:/usuario/listar";
+    }
+
+    @GetMapping("/listar")
+    public String listaUsuario(org.springframework.ui.Model model){
+        model.addAttribute("usuarios", service.getAll());
+        return "usuario/list";
     }
 
 }
